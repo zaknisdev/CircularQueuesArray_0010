ca8c2bd746c744ce72cd62a85566756870ebeb2e
zaknisdev <zakianisf1710@gmail.com>
2025-06-12 10:14:20 +0700

    membuat func jika rear di posisi terakhir array maka kembali ke awal dan menyimpan nilai num dalam rear dari array

--- queues.cpp
+++ queues.cpp
@@ -21,5 +21,12 @@ class Queues {
             FRONT = 0;
             REAR = 0;
         }
+        else {
+            if (REAR == max - 1)
+                REAR = 0;
+            else  
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 }
