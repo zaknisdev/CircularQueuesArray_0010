93d4ad7a6c2843298b0542b1c822fc3746a03730
zaknisdev <zakianisf1710@gmail.com>
2025-06-12 10:04:06 +0700

    inisiasi front sama rear pada posisi indeks array -1

--- queues.cpp
+++ queues.cpp
@@ -4,4 +4,10 @@ using namespace std;
 class Queues {
     int FRONT, REAR, max = 5;
     int queue_array[5];
+
+    public: 
+        Queues() {
+            FRONT = -1;
+            REAR = -1;
+        }
 }
