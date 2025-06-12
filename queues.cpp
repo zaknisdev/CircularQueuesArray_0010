705e2af1e8cd80e5827049c6e92e88ca8e8b2325
zaknisdev <zakianisf1710@gmail.com>
2025-06-12 10:52:27 +0700

    membuat switch case untuk menu pilihan program menggunakan try catch exception

--- queues.cpp
+++ queues.cpp
@@ -85,4 +85,40 @@ class Queues {
 int main() {
     Queues q;
     char ch;
+
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch) {
+                case '1': {
+                    q.insert();
+                    break;
+                }
+                case '2': {
+                    q.remove();
+                    break;
+                }
+                case '3': {
+                    q.display();
+                    break;
+                }
+                case '4': {
+                    return 0;
+                }
+            }
+            
+        }
+        catch (exception& e) {
+            cout << "Check for the values entered." << endl;
+        }
+    }
+    return 0;
 }
