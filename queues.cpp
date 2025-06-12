7772c430077b5754abe7813b474ee3b309d11d45
zaknisdev <zakianisf1710@gmail.com>
2025-06-12 10:40:30 +0700

    membuat iterasi dari awal array hingga akhir array

--- queues.cpp
+++ queues.cpp
@@ -72,6 +72,12 @@ class Queues {
                 FRONT_position++;
             }
             FRONT_position = 0;
+
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
-}
+};
