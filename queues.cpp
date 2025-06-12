72adbbe7cd27295c83d0af22240ddd7867d06588
zaknisdev <zakianisf1710@gmail.com>
2025-06-12 10:37:10 +0700

    jika posisi front lebih kecil dari rear maka iterasi dari FRONT hingga akhir array

--- queues.cpp
+++ queues.cpp
@@ -64,6 +64,14 @@ class Queues {
                 cout << queue_array[FRONT_position] << "   ";
                 FRONT_position++;
             }
+            cout << endl;
+        }
+        else {
+            while ( FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
         }
     }
 }
