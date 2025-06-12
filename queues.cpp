92ea9ff2cb71cd07bb9fdbb64e9f7bb947c908ef
zaknisdev <zakianisf1710@gmail.com>
2025-06-12 10:34:17 +0700

    membuat func if jika posisi front lebih kecil atau sama dengan rear

--- queues.cpp
+++ queues.cpp
@@ -58,5 +58,12 @@ class Queues {
             return;
         }
         cout << "\nElements in the queue are...\n";
+
+        if (FRONT_position <= REAR_position) {
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+        }
     }
 }
